aa9a821f02ed7d45bbaa57d4d178d7da9d237d46
edean78 <edean78@gmail.com>
2019-11-18 21:46:36 -0500

    Created the get and post request in the apiRoutes.js

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,39 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to the "data" sources.
+// This data sources hold an array of information on friends-data
+// ===============================================================================
+
+var friendData = require("../data/friends.js");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    // Create New Characters - takes in JSON input
+    app.post("/api/friends", function (req, res) {
+
+        var newFriend = req.body;
+
+        console.log(newFriend);
+
+        // Add the json the user sent to the friends array
+        friendData.push(newFriend);
+
+        // We then display the JSON to the users
+        res.json(newcharacter);
+    });
+
+}
