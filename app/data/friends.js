912f4846fba9a67099e3dc7616eebe23a83e7f12
edean78 <edean78@gmail.com>
2019-11-18 21:03:57 -0500

    Added friends array in the friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+// ========================================================
+// DATA
+// Below data will hold all of the friends.
+// Initially I just set it equal to a generic friend.
+// =========================================================
+ 
+
+var friends = [
+    {
+        name: "",
+        photo: "",
+        scores:[]
+    }
+];
+    
+
+// Export the array. This makes it accessible to other files using require.
+module.exports = friendsArray;
